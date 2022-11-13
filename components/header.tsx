feae4099a6224e7ee53fa64f651559dfb20b0705
Chima Ilo <collins.chima.ilo@gmail.com>
2022-11-13 12:16:40 +0100

    reset theme

--- components/header.tsx
+++ components/header.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 import Image from "next/image";
 import clsx from "clsx";
 import NavItem from "./nav";
-import Sidebar from "./Sidebar";
+import Sidebar from "./sidebar";
 import Portal from "./portal";
 import { NAVLINKS } from "../lib/constants";
 import { useTheme } from "./theme";
 import { MoonIcon, SunIcon } from "./svg/icons";
-import CircledButton from "./CircledButton";
+import IconButton from "./button/icon-button";
 
 export default function Header() {
   const [open, setOpen] = useState<boolean>(false);
@@ -28,12 +28,12 @@ export default function Header() {
       <header
         role="banner"
         className={clsx(
-          "flex flex-shrink-0 z-[4] sticky top-0 justify-center items-center h-14 sm:h-16 w-full transition-colors", theme === "dark" ? "bg-main" : "bg-slate-50"
+          "flex flex-shrink-0 z-[4] sticky top-0 justify-center items-center h-14 sm:h-16 md:h-20 w-full transition-colors", theme === "dark" ? "bg-main" : "bg-slate-50"
         )}
       >
         <div className="px-4 sm:px-6 lg:px-8 h-14 flex items-center max-w-screen-xl w-full">
           <Link href="/">
-            <a className="mr-8 leading-[0] capitalize font-serif text-3xl sm:text-5xl dark:text-slate-50 text-main font-semibold">
+            <a className="mr-8 leading-[0] capitalize font-serif text-xl sm:text-2xl md:text-4xl dark:text-slate-50 text-main font-semibold">
               Chima Ilo
             </a>
           </Link>
@@ -48,27 +48,26 @@ export default function Header() {
             </ul>
           </nav>
           <div className="flex items-center gap-2 sm:gap-4 ml-4 md:ml-12">
-            <CircledButton type="button" onChange={onChange}>
+            <IconButton handleClick={onChange}>
               {theme === "dark" ? (
-                <SunIcon svgClass="dark:text-slate-300 w-4 h-4 sm:w-5 sm:h-5" />
+                <SunIcon svgClass="dark:text-slate-400 dark:group-hover:text-slate-100 w-4 h-4 sm:w-5 sm:h-5 transition-colors duration-700" />
               ) : (
-                <MoonIcon svgClass="text-slate-700 w-4 h-4 sm:w-5 sm:h-5" />
+                <MoonIcon svgClass="text-slate-600 w-4 h-4 sm:w-5 sm:h-5 group-hover:text-main transition-colors duration-700" />
               )}
-            </CircledButton>
-            <CircledButton
-              type="button"
+            </IconButton>
+            <IconButton
               id="menubutton"
-              onClick={() => setOpen(!open)}
+              handleClick={() => setOpen(!open)}
               classes="md:hidden"
               aria-haspopup="true"
               aria-controls="menu"
             >
               <div className="w-5 h-5 relative">
-                <span className={clsx("h-0.5 dark:bg-slate-300 bg-slate-700 absolute block right-0 w-[120%] top-0 transition-opacity", open ? "opacity-0" : "opacity-1")}></span>
-                <span className={clsx("w-full h-0.5 dark:bg-slate-300 bg-slate-700 absolute block top-1/2 right-0 transition-transform duration-500", open ? "-rotate-[225deg]" : "")}></span>
-                <span className={clsx("h-0.5 dark:bg-slate-300 bg-slate-700 absolute block right-0 transition-transform bottom-0 duration-500", open ? "rotate-[225deg] w-full" : "w-[80%]")}></span>
+                <span className={clsx("h-0.5 dark:bg-slate-400 dark:group-hover:bg-slate-100 bg-slate-600 group-hover:bg-main absolute block right-0 w-[120%] top-0 transition duration-700", open ? "opacity-0" : "opacity-1")}></span>
+                <span className={clsx("w-full h-0.5 dark:bg-slate-400 dark:group-hover:bg-slate-100 bg-slate-600 group-hover:bg-main absolute block top-1/2 right-0 transition duration-700", open ? "-rotate-[225deg]" : "")}></span>
+                <span className={clsx("h-0.5 dark:bg-slate-400 dark:group-hover:bg-slate-100 bg-slate-600 group-hover:bg-main absolute block right-0 transition duration-700", open ? "rotate-[225deg] w-full top-1/2" : "w-[80%] bottom-0")}></span>
               </div>
-            </CircledButton>
+            </IconButton>
           </div>
         </div>
       </header>
