78ff3f0229ade073b0b814583ce83b24327c4230
chimailo <collins.chima.ilo@gmail.com>
2023-08-25 05:25:08 +0100

    custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
   darkMode: "class",
   theme: {
     screens: {
-      sm: 480 + 32 + "px",
+      sm: 576 + 24 + "px",
       md: 768 + 32 + "px",
-      lg: 960 + 64 + "px",
+      lg: 960 + 48 + "px",
       xl: 1152 + 64 + "px",
     },
     extend: {
       colors: {
-        main: "#13233D",
+        "main-100": "#E6F4F2",
+        "main-200": "#A6D8DE",
+        "main-300": "#79C4CD", // links, dark
+        "main-400": "#017f76", // links, light
+        "main-500": "#344f60", // or #2F4858
+        "main-600": "#22343f",
+        "main-700": "#162127", // or #1D2C35
       },
       fontFamily: {
-        'sans': ['Asap', ...defaultTheme.fontFamily.sans],
-        'serif': ['Playfair Display', ...defaultTheme.fontFamily.serif],
+        sans: ["var(--font-asap)", ...defaultTheme.fontFamily.sans],
+        serif: ["var(--font-playfair)", ...defaultTheme.fontFamily.serif],
       },
       spacing: {
         28: "7rem",
