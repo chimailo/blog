d9b7c1b1323f43371863c50eba1b5454623c4dba
chimailo <collins.chima.ilo@gmail.com>
2023-08-29 06:48:14 +0100

    hero component

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,31 +1,31 @@
-import ArrowLink from "./arrow-link"
-import { Button, IconButton } from "./button"
-import { EMAIL, GITHUB, LINKEDIN, RESUME, TWITTER } from "../lib/constants"
-import { GithubIcon, LinkedinIcon, MailIcon, TwitterIcon } from "./svg/icons"
+import ArrowLink from "./arrow-link";
+import { Button, IconButton } from "./button";
+import { EMAIL, GITHUB, LINKEDIN, RESUME, TWITTER } from "../lib/constants";
+import { GithubIcon, LinkedinIcon, MailIcon, TwitterIcon } from "./svg/icons";
 
 export default function Hero() {
   const contactLinks = [
     {
       link: `mailto:${EMAIL}`,
-      icon: MailIcon
+      icon: MailIcon,
     },
     {
       link: GITHUB,
-      icon: GithubIcon
+      icon: GithubIcon,
     },
     {
       link: LINKEDIN,
-      icon: LinkedinIcon
+      icon: LinkedinIcon,
     },
     {
       link: TWITTER,
-      icon: TwitterIcon
+      icon: TwitterIcon,
     },
-  ]
+  ];
 
   return (
-    <header className="md:h-[calc(100vh_-_80px)] h-[calc(100vh_-_56px)] flex flex-col justify-center items-start">
-      <div className='flex items-center gap-2'>
+    <header className="md:py-16 py-8 sm:grid sm:grid-cols-2">
+      {/* <div className='flex items-center gap-2'>
         <span className='w-[min(20vw,_180px)] h-0.5 bg-slate-300 dark:bg-slate-600'></span>
         <div className='flex items-center'>
           {contactLinks.map((contact, i) =>
@@ -34,21 +34,22 @@ export default function Hero() {
             </IconButton>
           )}
         </div>
-      </div>
-      <div className="max-w-4xl my-4 pl-5 sm:pl-10 md:pl-16 lg:pl-20">
-        <h1 className='text-[clamp(25px,_5.5vw,_52px)] text-main font-serif font-bold dark:text-slate-50'>
-          Hi there, I&apos;m Chima Ilo.
+      </div> */}
+      <div className="my-4">
+        <h1 className="text-[clamp(32px,_5.5vw,_44px)] mb-6 md:mb-9 text-primary-400 font-serif font-bold dark:text-slate-50">
+          Hello, I&apos;m Chima.
         </h1>
-        <h2 className='text-[clamp(25px,_5.5vw,_52px)] text-slate-700 font-serif font-bold dark:text-slate-300'>
-          I build excellent user experiences in React, Vue and Typescript.
-        </h2>
-        <div className="flex items-center gap-6 md:gap-10 mt-6 md:mt-10">
+        <p className="text-slate-700 dark:text-secondary-100 text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo.
+        </p>
+        {/* <div className="flex items-center gap-6 md:gap-10 mt-6 md:mt-10">
           <Button classes="">view projects</Button>
           <ArrowLink href={RESUME} external>resume</ArrowLink>
-        </div>
+        </div> */}
       </div>
     </header>
-  )
+  );
 }
-
-
