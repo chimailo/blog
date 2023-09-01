da381f86c370220b9f259b8599f915fc71d2dcce
chimailo <collins.chima.ilo@gmail.com>
2023-09-01 07:02:53 +0100

    page layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,29 +1,55 @@
+import { useEffect } from "react";
+import { Asap, Playfair_Display } from "next/font/google";
 import Footer from "./footer";
 import Header from "./header";
 import Meta from "./meta";
-import Spacer from "./spacer";
 import { ThemeProvider } from "./theme";
+import Container from "./container";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export default function Layout({ children }: Props) {
+const asap = Asap({
+  weight: ["400", "500", "600"],
+  style: ["normal"],
+  subsets: ["latin"],
+  variable: "--font-asap",
+  display: "swap",
+});
+
+const playfair = Playfair_Display({
+  weight: ["400", "500", "600", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  variable: "--font-playfair",
+  display: "swap",
+});
+
+const Layout = ({ children }: Props) => {
+  useEffect(() => {
+    const body = document.getElementById("body");
+    body.classList.add(asap.variable);
+    body.classList.add(playfair.variable);
+  }, []);
+
   return (
     <>
       <Meta />
       <ThemeProvider>
-        <div className="min-h-screen bg-slate-50 dark:bg-main text-main transition dark:text-slate-300 z-[2]">
-          <a href="#main" className="sr-only focus:not-sr-only focus:top-6 focus:left-6 focus:z-50 focus:absolute focus:outline-0 focus:p-4 border-solid border-main border-2 rounded dark:border-slate-50">
-            Skip to content
-          </a>
-          <Header />
-          <main id="main">
-            {children}
-          </main>
-        </div>
+        <a
+          href="#main"
+          className="sr-only focus:not-sr-only focus:top-6 focus:left-6 focus:z-50 focus:absolute focus:outline-0 focus:p-4 border-solid border-primary-400 border-2 rounded dark:border-slate-50"
+        >
+          Skip to content
+        </a>
+        <Header />
+        <Container>{children}</Container>
         <Footer />
+        <div id="drawer" />
       </ThemeProvider>
     </>
   );
 };
+
+export default Layout;
