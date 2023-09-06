7a8cdce057003684ae11eb77ab43ca8ac8626bf5
chimailo <collins.chima.ilo@gmail.com>
2023-09-06 18:42:18 +0100

    navbar component

--- components/nav.tsx
+++ components/nav.tsx
@@ -12,14 +12,15 @@ type Props = {
 const NavItem = ({ classes, item }: Props) => {
   return (
     <li role="none" className="my-2">
-      <Link href={item.link}>
-        <a
-          role="menuitem"
-          className={`capitalize font-medium w-full whitespace-nowrap leading-none text-slate-600 hover:text-main dark:text-slate-300 block transition-all dark:hover:text-slate-50 ${classes}`}
-        >
-          {item.name}
-          {item.external && <ExternalLinkIcon svgClass="text-inherit w-3 h-3 ml-1" />}
-        </a>
+      <Link
+        role="menuitem"
+        href={item.link}
+        className={`capitalize font-medium w-full whitespace-nowrap leading-none text-slate-500 hover:text-secondary-300 dark:text-secondary-100 block transition-all motion-reduce:transition-none motion-reduce:hover:text-secondary-300 motion-reduce:dark:hover:text-slate-50 dark:hover:text-slate-50 ${classes}`}
+      >
+        {item.name}
+        {item.external && (
+          <ExternalLinkIcon svgClass="text-inherit w-3 h-3 ml-1" />
+        )}
       </Link>
     </li>
   );
